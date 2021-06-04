07e0606f137e7b241bce93a597a4fe2495ad6eb8
swill <swilltec@gmail.com>
2021-06-04 12:58:12 +0000

    refactor: replace getServerSideProps with getStaticPaths and getStaticProps

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,29 +1,39 @@
-import Link from "next/link"
+import Link from "next/link";
 
-const article = ({article}) => {
-  return(
-      <>
-      <h1>
-          {article.title}
-      </h1>
+const article = ({ article }) => {
+  return (
+    <>
+      <h1>{article.title}</h1>
       <p>{article.body}</p>
       <br />
-      <Link href='/'>Go Back</Link>
-      </>
-
-  )
+      <Link href="/">Go Back</Link>
+    </>
+  );
 };
 
-export const getServerSideProps = async (context) => {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+export const getStaticProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  );
 
-    const article = await res.json()
-    return {
-        props: {
-            article
-        }
+  const article = await res.json();
+  return {
+    props: {
+      article,
+    },
+  };
+};
 
-    }
-}
+export const getStaticPaths = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+
+  const articles = await res.json();
+  const ids = articles.map((article) => article.id);
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+  return {
+    paths,
+    fallback: false,
+  };
+};
 
 export default article;
