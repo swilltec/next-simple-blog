d741b066515ff0d06d203fda41d6ced0c76a2963
swill <swilltec@gmail.com>
2021-06-04 12:30:34 +0000

    feat: add articleitem

---
+++ components/ArticleItem.js
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import React from 'react'
+import articleStyle from '../styles/Article.module.css'
+
+
+function ArticleItem({article}) {
+    return (
+        <Link href="/article/[id]" as={`/article/${article.id}`}>
+            <a className={articleStyle.card}>
+                <h3>
+                    {article.title} &rarr;
+                </h3>
+                <p>{article.body}</p>
+            </a>
+        </Link>
+    )
+}
+
+export default ArticleItem
--- components/ArticleList.js
+++ components/ArticleList.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import articleStyle from '../styles/Article.module.css'
+import ArticleItem from './ArticleItem'
 
 const ArticleList = ({articles}) => {
     return (
         <div className={articleStyle.grid}>
-            {articles.map(article => <h3 key={article.id}>{article.title}</h3>) }
+            {articles.map(article =>
+                <ArticleItem key={article.id} article={article} />
+            )
+            }
         </div>
     )
 }
